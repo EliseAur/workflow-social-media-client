7622e485b0d88b04ae30b2fe819978679a933971
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-02-01 22:41:19 +0100

    Formatting all js files with prettier and new tabsize/indentation

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,12 @@ import ui from "./ui/index.js";
 
 ui();
 router();
+
+export const templateInstance = (id = "404") => {
+  const template = document.querySelector(`template#${id}`);
+  if (template) {
+    return template.content.cloneNode(true);
+  }
+
+  throw new Error(`Template #${id} not found`);
+};

cecafbff8185f06caa62155fa094ee8c3c2d94d2
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-02-13 16:56:29 +0100

    Changing the order of the tests

--- cypress/e2e/invalidLogin.cy.js
+++ cypress/e2e/invalidLogin.cy.js
@@ -3,6 +3,16 @@ describe("invalidLogin", () => {
     cy.visitHome();
   });
 
+  it("shows a message if the user try to submit the login form with invalid email", () => {
+    cy.showRegisterForm();
+
+    cy.showLoginForm();
+
+    cy.loginWithInvalidCredentials();
+
+    cy.showAlertMessageForInvalidEmail();
+  });
+
   it("shows a message if the user try to submit the login form with invalid password", () => {
     cy.showRegisterForm();
 
@@ -13,20 +23,6 @@ describe("invalidLogin", () => {
     cy.loginWithInvalidPassword();
 
     cy.showAlertMessageForInvalidPassword();
-
-    cy.checkNetworkResponseInvalidLogin();
-  });
-
-  it("shows a message if the user try to submit the login form with invalid email", () => {
-    cy.showRegisterForm();
-
-    cy.showLoginForm();
-
-    cy.interceptInvalidLogin();
-
-    cy.loginWithInvalidCredentials();
-
-    cy.showAlertMessageForInvalidEmail();
 
     cy.checkNetworkResponseInvalidLogin();
   });

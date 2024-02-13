0030b6ff0132656cfdf9b092229dcf70f9cf67c5
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-02-13 15:45:43 +0100

    Edit for .env and remove dead code in cypress config file

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,27 +1,16 @@
-// import { defineConfig } from "cypress";
 require("dotenv/config");
 const { defineConfig } = require("cypress");
-// require("dotenv").config();
 
 module.exports = defineConfig({
   e2e: {
-    // Cypress globals
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
     password: process.env.USER_PASSWORD,
+    name: process.env.USER_NAME,
+    email: process.env.USER_EMAIL,
+    avatar: process.env.USER_AVATAR,
   },
 });
-
-// export default defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-//   env: {
-//     password: process.env.USER_PASSWORD,
-//   },
-// });
